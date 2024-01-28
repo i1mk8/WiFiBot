32620daea1cb97ca45ceaef771480117e1b9274a
i1mk8 <i1mk8.ln@gmail.com>
2024-01-28 15:02:56 +0500

    Исправлены ошибки в формировании конфига

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ read -p "Введите токен бота: " BOT_TOKEN
 read -p "Введите user id (если вы хотите добавить несколько аккаунтов, то введите их user id через запятую): " BOT_USERS
 read -p "Введите временную зону (список - https://github.com/Lewington-pitsos/golang-time-locations): " TIMEZONE
 
-echo "{\"bot_token\": \"${BOT_TOKEN}\", \"bot_users\": ${BOT_USERS}, \"timezone\": \"${TIMEZONE}\", \"schedule_enabled\": false, \"schedule_down_hour\": 0, \"schedule_down_minute\": 0, \"schedule_up_hour\": 0, \"schedule_up_minute\": 0}" > "${WORK_DIR}/wifi_bot.json"
+echo "{\"bot_token\": \"${BOT_TOKEN}\", \"bot_users\": [${BOT_USERS}], \"timezone\": \"${TIMEZONE}\", \"schedule_enabled\": false, \"schedule_down_hour\": 0, \"schedule_down_minute\": 0, \"schedule_up_hour\": 0, \"schedule_up_minute\": 0}" > "${WORK_DIR}/wifi_bot.json"
 
 EXECUTABLE="${WORK_DIR}/on-startup.sh"
 wget -O $EXECUTABLE https://raw.githubusercontent.com/i1mk8/WiFiBot/master/on-startup.sh
