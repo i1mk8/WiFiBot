0264617b57c9ee99f12e3d6ee29fe0859b2e1acd
i1mk8 <i1mk8.ln@gmail.com>
2024-02-08 20:23:39 +0500

    Исправлены недоработки в установщике

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,15 @@ mkdir $WORK_DIR
 read -p "Введите токен бота: " BOT_TOKEN
 read -p "Введите user id (если вы хотите добавить несколько аккаунтов, то введите их user id через запятую): " BOT_USERS
 
-echo "{\"bot_token\": \"${BOT_TOKEN}\", \"bot_users\": [${BOT_USERS}], \"schedule_enabled\": false, \"schedule_down_hour\": 0, \"schedule_down_minute\": 0, \"schedule_up_hour\": 0, \"schedule_up_minute\": 0}" > "${WORK_DIR}/wifi_bot.json"
+echo "{\"bot_token\": \"${BOT_TOKEN}\", \"bot_users\": [${BOT_USERS}], \"schedule_enabled\": false, \"schedule_down_hour\": 0, \"schedule_down_minute\": 0, \"schedule_up_hour\": 0, \"schedule_up_minute\": 0}" > "${WORK_DIR}/config.json"
 
 EXECUTABLE="${WORK_DIR}/startup.sh"
-wget -O - https://raw.githubusercontent.com/i1mk8/WiFiBot/master/scripts/startup.sh >> $EXECUTABLE
+wget -O - https://raw.githubusercontent.com/i1mk8/WiFiBot/master/scripts/startup.sh > $EXECUTABLE
 chmod +x $EXECUTABLE
 
 CRON="/tmp/cron"
 crontab -l > $CRON
-echo "@reboot ${EXECUTABLE}" >> $CRON
+echo "@reboot sleep 60; ${EXECUTABLE}" >> $CRON # Задержка необходима для полной инициализации системы и корректного запуска
 crontab $CRON
 
 fs save
